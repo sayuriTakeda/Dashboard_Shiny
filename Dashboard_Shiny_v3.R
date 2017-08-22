ac2f6b79d17e194e347cc619e3b9170e725eeda1
Sayuri <sayuri.takeda@hotmail.com>
2017-08-22 18:41:05 -0300

    Update Dashboard_Shiny_v3.R

--- Dashboard_Shiny_v3.R
+++ Dashboard_Shiny_v3.R
@@ -1,105 +1,112 @@
 library(shiny)
 library(shinydashboard)
-library(ggplot2)
-library(plotly)
-library(dplyr)
-library(highcharter)
 
-base <- read.csv2("/home/sayuritakeda/apriori.csv", 
-                  fileEncoding = "ISO-8859-1",
-                  stringsAsFactors = FALSE)
-base <- base[,-7]
-
-base$SUPPORT <- as.double(base$SUPPORT)
-base$CONFIDENCE <- as.double(base$CONFIDENCE)
-base$LIFT <- as.double(base$LIFT)
-
-
-df <- data.frame(
-  x = base$LHS,
-  y = base$RHS,
-  Lift = base$LIFT
-)
-
-
-ui <- dashboardPage(skin = "blue",
-                    dashboardHeader(title = "Teste"),
-                    dashboardSidebar(
-                      sidebarMenu(
-                        menuItem("Tabela", tabName = "tabela", icon = icon("th"),
-                                 badgeLabel = "new", badgeColor = "green"),
-                        menuItem("Gráficos", tabName = "Tabelas", icon = icon("area-chart"),
-                                 menuSubItem("Sub-item 1", tabName = "subitem1"),
-                                 menuSubItem("Sub-item 2", tabName = "subitem2")),
-                        menuItem("Help", tabName = "help", icon = icon("question"))
-                      )
-                    ),
-                    dashboardBody(
-                      tabItems(
-                        tabItem(tabName = "tabela", 
-                                fluidRow( 
-                                  infoBoxOutput("progressBox"),
-                                  infoBoxOutput("progressBox2"),
-                                  infoBoxOutput("progressBox3")
-                                ),  
-                                fluidRow(
-                                  dataTableOutput("base")
-                                )
-                        ),
-                        tabItem(
-                          tabName = "subitem1",plotlyOutput("plot1") 
-                                ),
-                        tabItem(
-                          tabName = "subitem2",h2("tabela2")
-                        ),
-                        tabItem(tabName = "help",h2("Help"),
-                                fluidRow(
-                                  box(
-                                    title = "SUPPORT",width = 4, background = "light-blue",
-                                    " % de cupons que possuem determinada combinação (LHS) "
-                                  ),
-                                  box(
-                                    title = "CONFIDENCE",width = 4, background = "light-blue",
-                                    "Percentual de ocorrência da regra (LHS -> RHS)"
-                                  ),
-                                  box(
-                                    title = "LIFT",width = 4, background = "light-blue",
-                                    "Quantas vezes aumenta a chance do RHS dado que o cliente levou o LHS"
-                                  )
-                                ))
-                      )
-                    ))
+ui <- dashboardPage(
+  dashboardHeader(title = "Teste",dropdownMenu(type = "notifications",
+                                               icon = icon("question"),
+                                               messageItem(
+                                                 from = "Sales Dept",
+                                                 message = "Sales are steady this month.",
+                                                 icon = icon("")
+                                               ),
+                                               messageItem(
+                                                 from = "Sales Dept",
+                                                 message = "Sales are steady this month.",
+                                                 icon = icon("")
+                                               ),
+                                               messageItem(
+                                                 from = "Sales Dept",
+                                                 message = "Sales are steady this month.",
+                                                 icon = icon("")
+                                               )
+  )),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("menuIt1", tabName = "graph", icon = icon("dashboard"),
+               badgeLabel = "new", badgeColor = "green"),
+      menuItem("menuIt2", tabName = "Tabelas", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    
+      # Also add some custom CSS to make the title background area the same
+      # color as the rest of the header.
+      tags$head(tags$style(HTML('
+                                /* logo */
+                                .skin-blue .main-header .logo {
+                                background-color: #2d5986;
+                                }
+                                
+                                /* logo when hovered */
+                                .skin-blue .main-header .logo:hover {
+                                background-color: #c2c2d6;
+                                }
+                                
+                                /* navbar (rest of the header) */
+                                .skin-blue .main-header .navbar {
+                                background-color: #2d5986;
+                                }        
+                                
+                                /* main sidebar */
+                                .skin-blue .main-sidebar {
+                                background-color: #19334d;
+                                }
+                                
+                                /* active selected tab in the sidebarmenu */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                                background-color: #a3a3c2;
+                                }
+                                
+                                /* other links in the sidebarmenu */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                                background-color: #a3a3c2;
+                                color: #000000;
+                                }
+                                
+                                /* other links in the sidebarmenu when hovered */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover{
+                                background-color: #c2c2d6;
+                                }
+                                /* toggle button when hovered  */                    
+                                .skin-blue .main-header .navbar .sidebar-toggle:hover{
+                                background-color: #c2c2d6;
+                                }
+                                '))),
+                
+    
+    
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 9),
+    tabItems(
+      tabItem(tabName = "graph", 
+              fluidRow(
+                infoBoxOutput("progressBox")),
+              fluidRow(
+                box(title = "title",
+                    solidHeader = TRUE,
+                    collapsible = TRUE,
+                    status = "primary", 
+                    plotOutput("plot1",height = 250))),
+              fluidRow(
+                box(title = "Inputs", 
+                    background = "blue",
+                    plotOutput("plot2",height = 250)))
+      ),
+      
+      tabItem(tabName = "Tabelas",h2("botar tabelas aqui"),
+              fluidRow(
+                box(
+                  title = "Title 6",width = 4, background = "maroon",
+                  "A box with a solid maroon background"
+                )
+              ))
+    )
+  ))
 
 server <- function(input, output, session) {
-  
-  output$base = renderDataTable({
-    base
-  })
-  
-  output$plot1 <- renderPlotly({
-    p<-ggplot(df, aes(x, y)) +
-    geom_point(aes(colour = Lift),size = 3) +
-    scale_colour_gradient(low = "#e6f0ff", high = "#001433") +
-    theme(axis.text.x=element_blank(),
-          axis.title.x=element_blank(),
-          axis.title.y=element_blank()) 
-    
-    ggplotly(p,  width = 1000, height = 700)})
-
+  output$plot1 <- renderPlot({plot(iris$Sepal.Length,iris$Sepal.Width)})
   output$plot2 <- renderPlot({plot(iris$Petal.Length,iris$Petal.Width)})
-  
-  output$progressBox <- renderInfoBox({infoBox("SUPPORT",
-                                               paste0("Min: ",round(min(base$SUPPORT),3)," /","\n",
-                                                      "Max: ",round(max(base$SUPPORT),3)),
-                                               color = "blue")})
-  output$progressBox2 <- renderInfoBox({infoBox("CONFIDENCE",
-                                                paste0("Min: ",round(min(base$CONFIDENCE),1)," /","\n",
-                                                       "Max: ",round(max(base$CONFIDENCE),1)),
-                                                color = "blue")})
-  output$progressBox3 <- renderInfoBox({infoBox("LIFT",
-                                                paste0("Min: ",round(min(base$LIFT),1)," /","\n",
-                                                       "Max: ",round(max(base$LIFT),1)),
-                                                color = "blue")})
+  output$progressBox <- renderInfoBox({infoBox("Progress",paste0(25+input$clusters,"%"),icon = icon("list"),color = "purple")})
 }
 
 shinyApp(ui, server)
